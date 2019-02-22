c1a361301e2502f693e1567c975a041050f358ef
BillyBonbon <bonbongames@gmail.com>
2019-02-22 12:23:00 +0000

    Added rudimentary dragging and snapping to grid on drop

---
+++ Assets/Scripts/ShapeMover.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeMover : MonoBehaviour
+{
+	[SerializeField]
+	SnapToGrid m_SnapToGrid = null;
+
+	private bool dragging = false;
+	private float distance;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+	void OnMouseDown()
+	{
+		distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+		dragging = true;
+	}
+
+	void OnMouseUp()
+	{
+		dragging = false;
+		m_SnapToGrid.Snap();
+	}
+
+	void Update()
+	{
+		if (dragging)
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			Vector3 rayPoint = ray.GetPoint(distance);
+			transform.position = rayPoint;
+		}
+
+	}
+}
+
---
+++ Assets/Scripts/SnapToGrid.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapToGrid : MonoBehaviour
+{
+	[SerializeField]
+	private float m_snapDistance = 0.5f;
+
+	public float SnapDistance { get => m_snapDistance; set => m_snapDistance = value; }
+
+	public void Snap()
+	{
+		Transform transform = gameObject.transform;
+		Vector3 position = transform.position;
+
+		position.x = FindNearestSnapValue(position.x);
+		position.y = FindNearestSnapValue(position.y);
+		transform.position = position;
+	}
+
+	private float FindNearestSnapValue(float position)
+	{
+		float snapValue = 0.0f;
+		float remainder = position % m_snapDistance;
+		bool roundUp = (remainder >= (m_snapDistance / 2));
+
+		snapValue = position - remainder;
+		if ( roundUp )
+		{
+			snapValue += m_snapDistance;
+		}
+
+		return snapValue;
+	}
+}
