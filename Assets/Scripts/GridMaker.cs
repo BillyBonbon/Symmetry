cc89c55a467b08dd8e268907311ff7ec8e4762d2
BillyBonbon <bonbongames@gmail.com>
2019-02-25 16:43:19 +0000

    Grid change

--- Assets/Scripts/GridMaker.cs
+++ Assets/Scripts/GridMaker.cs
@@ -10,13 +10,13 @@ public class GridMaker : MonoBehaviour
 	[SerializeField]
 	LineRenderer m_gridLine = null;
 
-	float screnWidth = 20.0f;
-	float screnHeight = 20.0f;
+	float screenWidth = 20.0f;
+	float screenHeight = 20.0f;
 
 	// Start is called before the first frame update
 	void Start()
     {
-		for (int i = 1; i < screnWidth; i++)
+		for (int i = 0; i < screenWidth; i++)
 		{
 			CreateGridline(false, i);
 			CreateGridline(false, -i);
@@ -27,7 +27,7 @@ public class GridMaker : MonoBehaviour
 
 	private void CreateGridline(bool horizonatal, int delta)
 	{
-		float length = horizonatal ? screnWidth : screnHeight;
+		float length = horizonatal ? screenWidth : screenHeight;
 
 		LineRenderer lineRenderer = Instantiate(m_gridLine, this.transform);
 		Vector3 start = new Vector3(horizonatal ? length : m_gridSize * delta, horizonatal ? m_gridSize * delta : length);
